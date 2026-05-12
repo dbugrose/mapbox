b6c19cd3dd7947b630f21b1ffbdea8bac273560d
dbugrose <darcy.ophelia.rose@gmail.com>
2026-05-12 09:01:24 -0700

    re-added upper border

--- app/page.tsx
+++ app/page.tsx
@@ -314,12 +314,10 @@ const regionColorMap: Record<string, [string, string]> = {
         loading="eager"
       />
     </picture>
-        <h2 className="border-b-2 border-b-[#FFC600] text-center text-lg font-bold pb-2">
-        </h2>
 
         {hoveredData ? (
           <div className="text-center openSans">
-            <h3 className="text-md font-extrabold mt-2">{hoveredData.Region}</h3>
+            <h3 className="text-md font-extrabold mt-2 border-t-[#FFC600] border-t-2 pt-2">{hoveredData.Region}</h3>
             <p className="text-[#002856] text-md font-bold pb-2 border-b-2 border-b-[#FFC600]">{hoveredData.County}</p>
             <div className="space-y-3 text-sm">
               <p>
